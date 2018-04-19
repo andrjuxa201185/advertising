eae600e8c5e6ac8badddd5a3a854bf3bec44bcdb
andrey <andrjuxa201185@gmail.com>
2018-04-20 00:10:55 +0300

    start add contact

--- static/js.js
+++ static/js.js
@@ -1,5 +1,14 @@
-// $(document).ready(function() {
-//     window.addEventListener('resize', function(){
-//         alert(window.innerWidth);
-//     })
-// });
+$(document).ready(function() {
+    window.addEventListener('resize', changeClassNavFixed);
+});
+
+
+function changeClassNavFixed(){
+    let nav = document.querySelector('#navbar');
+    if (window.innerWidth <= 768){
+        nav.classList.remove('fixed-top');
+    }
+    else{
+        nav.classList.add('fixed-top');
+    }
+}
