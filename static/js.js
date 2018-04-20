eb0c8bb2592eba230e662ed22a19427d5ab38845
andrey <andrjuxa201185@gmail.com>
2018-04-21 00:07:10 +0300

    finish conyact

--- static/js.js
+++ static/js.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
+    changeClassNavFixed();
     window.addEventListener('resize', changeClassNavFixed);
+    window.addEventListener('scroll', lessNavbar);
 });
 
 
@@ -7,8 +9,33 @@ function changeClassNavFixed(){
     let nav = document.querySelector('#navbar');
     if (window.innerWidth <= 768){
         nav.classList.remove('fixed-top');
+        document.getElementById('carouselExampleControls').style.marginTop = '0px';
     }
     else{
         nav.classList.add('fixed-top');
+        document.getElementById('carouselExampleControls').style.marginTop = '50px';
+    }
+}
+
+function lessNavbar(){
+    let scrY = window.pageYOffset;
+    let span = document.querySelector("h1 span");
+    let navbar = document.getElementById("navbar");
+    let h1 = document.querySelector("h1");
+    let carusel = document.getElementById('carouselExampleControls');
+    if (window.innerWidth > 768){
+        if (scrY > 80){
+            carusel.style.marginTop = '30px';
+            span.classList.add('display-hidden-span');
+            navbar.classList.add('less-navbar');
+            h1.classList.add('less-font-h1');
+            navbar.classList.add('navbar-expand-lg');
+        }else{
+            carusel.style.marginTop = '50px';
+            span.classList.remove('display-hidden-span');
+            navbar.classList.remove('less-navbar');
+            h1.classList.remove('less-font-h1');
+            navbar.classList.remove('navbar-expand-lg');
+        }
     }
 }
