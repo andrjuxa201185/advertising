bec21e9a0d4cf53a8e84b0cf283e12946502cf90
andrey <andrjuxa201185@gmail.com>
2018-04-22 23:39:39 +0300

    chenge js(lessnavbar576) add border carusel top

--- static/js.js
+++ static/js.js
@@ -1,20 +1,8 @@
 $(document).ready(function() {
-    // changeNavFixed();
     lessNavbar();
-    // window.addEventListener('resize', changeNavFixed);
     window.addEventListener('scroll', lessNavbar);
     window.addEventListener('resize', lessNavbar);
 });
-
-
-// function changeNavFixed(){
-//     let nav = document.querySelector('#navbar');
-//     if (window.innerWidth <= 768){
-//         nav.style.position = 'relative';
-//     }else{
-//         nav.style.position = 'fixed';
-//     }
-// };
 
 
 function lessNavbar(){
@@ -22,17 +10,22 @@ function lessNavbar(){
     let span = document.querySelector("h1 span");
     let navbar = document.getElementById("navbar");
     let h1 = document.querySelector("h1");
-    if (window.innerWidth > 768){
+    let carusel = document.getElementById('carouselExampleControls');
+
+    if (window.innerWidth > 576){
         if (scrY > 80){
             span.classList.add('display-hidden-span');
             navbar.classList.add('less-navbar');
             h1.classList.add('less-font-h1');
-            navbar.classList.add('navbar-expand-md');
+            if (window.innerWidth < 768){
+                h1.style.marginLeft = "20px";
+            }
+            navbar.classList.add('navbar-expand-sm');
         }else{
             span.classList.remove('display-hidden-span');
             navbar.classList.remove('less-navbar');
             h1.classList.remove('less-font-h1');
-            navbar.classList.remove('navbar-expand-md');
+            navbar.classList.remove('navbar-expand-sm');
         };
     };
 };
