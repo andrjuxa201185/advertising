85b6f98ee98c9ee8cc6458338544f56d8ce16717
andrey <andrjuxa201185@gmail.com>
2018-08-26 23:09:34 +0300

    26082018

--- static/border-amimation.js
+++ static/border-amimation.js
@@ -14,51 +14,64 @@ function BorderAnimation(elem){
         elem.appendChild(border[i]);
     };
 
-    elem.addEventListener("mouseover", borderShow.bind(null, 0, "height"));
+    elem.addEventListener("mouseenter", function(){
+        elem.style.border = "none";
+        showBorder0();
+    });
 
-
-    elem.addEventListener("mouseout", function(){
-        elem.style.border = '';
-        for (let i = 0; i > 4; i++) {
-            border[i].removeEventListener("transitionend", borderShow.bind(null, i, "height"));
-        };
+    elem.addEventListener("mouseleave", function(){
+        elem.style.border = "";
+        border[0].removeEventListener("transitionend", showBorder1);
+        border[1].removeEventListener("transitionend", showBorder2);
+        border[2].removeEventListener("transitionend", showBorder3);
         for (let i = 0; i < 4; i++) {
-            border[i].style.width = '';
-            border[i].style.height = '';
+            border[i].style.width = "";
+            border[i].style.height = "";
         };
     });
 
+    function showBorder0(){
+        border[0].style.height = "100%";
+        border[0].addEventListener("transitionend", showBorder1);
+    };
 
-    function borderShow(i, style){
-        elem.style.border = "none";
-        if (i < 0 || i > 3) return;
-        border[i].style[style] = "100%";
-        style = style == "height" ? "width" : "height";
-        border[i].addEventListener("transitionend", borderShow.bind(null, ++i, style));
+    function showBorder1(){
+        border[1].style.width = "100%";
+        border[1].addEventListener("transitionend", showBorder2);
+    };
+
+    function showBorder2(){
+        border[2].style.height = "100%";
+        border[2].addEventListener("transitionend", showBorder3);
+    };
+    function showBorder3(){
+        border[3].style.width = "100%";
     };
 
 
-    // elem.addEventListener("mouseover", function(){
-    //     elem.style.border = "none";
-    //     border[0].style.height = "100%";
-    //     border[0].addEventListener("transitionend", borderShow0.bind(null, "la"));
-    // });
+
+    // elem.addEventListener("mouseover", borderShow.bind(null, 0, "height"));
 
     // elem.addEventListener("mouseout", function(){
-    //     border[0].removeEventListener("transitionend", borderShow0);
-    //     border[0].style.height = "";
+    //     elem.style.border = '';
+    //     for (let i = 0; i > 4; i++) {
+    //         border[i].removeEventListener("transitionend", borderShow.bind(null, i, "height"));
+    //     };
+    //     for (let i = 0; i < 4; i++) {
+    //         border[i].style.width = '';
+    //         border[i].style.height = '';
+    //     };
     // });
 
-
-    // function borderShow0(p){
-    //     alert('dgyjfy ' + p);
-    // };
+    function showBorder(i, style){
+        elem.style.border = "none";
+        border[i].style[style] = "100%";
+    };
 };
 
 
-
-
-
+// style = style == "height" ? "width" : "height";
+// border[i].addEventListener("transitionend", showBorder(null, ++i, style));
 
 window.addEventListener('DOMContentLoaded', function(){
     let button = document.getElementsByClassName("button-on-scroll")[0];
